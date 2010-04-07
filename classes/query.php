e108c7c8f7a084fd3ac494a6b1576283bed78405
approots <rickjolly@hotmail.com>
2010-04-07 10:54:15 -0700

    small Query class changes

--- classes/query.php
+++ classes/query.php
@@ -1,20 +1,19 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
- * Database query object creation helper methods. Extends Kohana DB.
+ * Database query object creation helper methods. Extends Kohana_DB.
  * 
  * Modifications by Rick Jolly: 
- * - Added method sql($sql) as an alias to DB:query(NULL, $sql)
+ * - Added method sql($sql) as an alias to DB:query($any_type, $sql)
  *
  * @package    Database
  * @author     Kohana Team
  * @copyright  (c) 2009 Kohana Team
  * @license    http://kohanaphp.com/license
  */
-class Query extends DB {
+class Query extends Kohana_DB {
 	
 	public static function sql($sql)
 	{	
-		return new Database_Query(null, $sql);
+		return new Database_Query(NULL, $sql);
 	}
-
 }
